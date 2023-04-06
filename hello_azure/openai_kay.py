64169345f58e8c7040c7cb74108595ad2095ca78
412845222 <412845222@qq.com>
2023-04-06 22:06:43 +0800

    2023-4-6

---
+++ hello_azure/openai_kay.py
@@ -0,0 +1,2 @@
+
+api_key = "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"
--- hello_azure/urls.py
+++ hello_azure/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+from hello_azure import api
 urlpatterns = [
     path('', views.index, name='index'),
     path('hello', views.hello, name='hello'),
+    path('chatgpt/',api.chatgpt)
 ]
